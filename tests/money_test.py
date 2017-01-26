a9a02038635d9b87585a57af061429f52919e87a
Tomas Zahradnik <zahradnik91@gmail.com>
2017-01-26 14:53:30 +0100

    Fixing missing "USD" value in test for Money.supported_currencies

--- tests/money_test.py
+++ tests/money_test.py
@@ -83,7 +83,7 @@ class TestSupportedCurrencies(unittest.TestCase):
         expected = ["AUD","BGN","BRL","CAD","CHF","CNY","CZK","DKK","GBP",
             "HKD","HRK","HUF","IDR","ILS","INR","JPY","KRW","MXN","MYR",
             "NOK","NZD","PHP","PLN","RON","RUB","SEK","SGD","THB","TRY",
-            "ZAR","EUR"]
+            "ZAR","EUR", "USD"]
         print(result)
         self.assertCountEqual(expected, result)
 
