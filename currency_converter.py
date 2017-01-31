fefe731d1b7ddfd16402a89c58f4d4881b1e5ae3
mrzahrada <zahradnik91@gmail.com>
2017-01-31 16:19:33 +0100

    currency_converter can parse arguments

--- currency_converter.py
+++ currency_converter.py
@@ -1,11 +1,19 @@
 #! /usr/bin/env python
 # -*- coding: utf-8 -*-
 
-import sys
+import argparse
+import json
+from currency_converter.money import Money
 
-
-def main(argv):
-    pass
+def main(args):
+    m = Money()
+    result = m.convert(args.amount, args.input_currency, args.output_currency)
+    print(json.dumps(result, indent=4, sort_keys=True))
 
 if __name__ == '__main__':
-    main(sys.argv)
+    parser = argparse.ArgumentParser(description='Process some integers.')
+    parser.add_argument('--amount', type=float, help="amount", required=True)
+    parser.add_argument('--input_currency', type=str,  help="amount", required=True)
+    parser.add_argument('--output_currency', type=str, help="amount", required=False)
+    args = parser.parse_args()
+    main(args)
